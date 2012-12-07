5f9301116cc6671180d97c99b35835b60213f5d9
Mirko Mariotti <mirko@mirkomariotti.it>
2012-12-07 13:03:07 +0100

    Graph import links works

--- dssim.c
+++ dssim.c
@@ -73,11 +73,23 @@ void version()
 	fflush(stdout);
 }
 
+// Search the id of the node given its host address
+int search_node_id(Agnode_t ** ithnode, Agnode_t * inode,int nodes)
+{
+	int i;
+	for (i=0;i<nodes;i++) {
+		if (inode==*(ithnode+i)) {
+			return i;
+		}
+	}
+	return -1;
+}
 
 int main( int argc, char* argv[] )
 {
-	// Options parsing variables 
+	// Counters
 	int c,index;
+	int i,j,k,l;
 
 	// The protocol filename
 	char * protocol_file = NULL;
@@ -95,12 +107,28 @@ int main( int argc, char* argv[] )
 	graph_t * dsgraph;
 
 	Agnode_t * inode;
+	Agedge_t * iedge;
 
 	// File counter
 	FILE *fp;
 
 	// Verbose flag
 	char verbose=0;
+
+	// Node numbers
+	unsigned int nodes=0;
+
+	// Node index to Node structure
+	Agnode_t ** ithnode;
+
+ 	// The links matrix is a nodes x nodes which stores 0 if there in not a link x->y, !=0 otherwise (in the future it may contain same sort of link weight)
+	int * links;
+
+
+	///// Program start
+
+	// Size, in bytes, of each vector
+	size_t bytes = sizeof(int);
 
 	// Start with the command line parsing
 	while ((c = getopt (argc, argv, "vVk:p:g:i:")) != -1)
@@ -155,7 +183,7 @@ int main( int argc, char* argv[] )
 
 	if (verbose) {
 		version();
-		printf("-----\n\n");
+		printf("\n-----\n\n");
 	}
 
 	// Check the operation mode
@@ -172,8 +200,51 @@ int main( int argc, char* argv[] )
 
 		dsgraph=agread(fp);
 
-		for (inode=agfstnode(dsgraph);inode!=NULL;inode=agnxtnode(dsgraph,inode))
-		if (verbose) printf("Importing node: %s\n",inode->name);
+		if (verbose) printf("Importing graph nodes:\n");
+
+		nodes=agnnodes(dsgraph);
+
+		// Allocate memory for ithnode
+		ithnode = (Agnode_t **) malloc(nodes*sizeof(Agnode_t *));
+
+		// Allocate memory for the link matrix and initialize it
+		links = (int*)malloc(nodes*nodes*bytes);
+		for (i=0;i<nodes*nodes;i++) *(links+i)=0;
+
+		// Populate the id->node resolution
+		for (inode=agfstnode(dsgraph),i=0;inode!=NULL;inode=agnxtnode(dsgraph,inode),i++) {
+			if (verbose) printf(" - Importing node - %s (Host %p)\n",inode->name,inode);
+			*(ithnode+i)=inode;
+		}
+
+		if (verbose) printf("%d nodes imported\n\n",nodes);
+
+		if (verbose) printf("Importing graph edges:\n");
+
+		for (inode=agfstnode(dsgraph);inode!=NULL;inode=agnxtnode(dsgraph,inode)) {
+			if (verbose) printf(" - Importing node %s edges:\n",inode->name);
+			for (iedge=agfstout(dsgraph,inode);iedge!=NULL;iedge=agnxtout(dsgraph,iedge)) {
+				if (verbose) printf("   - Importing edge - %s (Host %p) -> %s (Host %p)\n",iedge->tail->name,iedge->tail,iedge->head->name,iedge->head);
+				// Find the two indexes
+				j=search_node_id(ithnode,iedge->tail,nodes);
+				k=search_node_id(ithnode,iedge->head,nodes);
+				*(links+j*nodes+k)=1;
+			}
+		}
+
+		if (verbose) printf("%d edges imported\n\n",agnedges(dsgraph));
+
+		if (verbose) {
+			printf("Links matrix:\n");
+			for (i=0;i<nodes;i++) {
+				printf(" ");
+				for (j=0;j<nodes;j++) {
+					printf("%d ",*(links+i*nodes+j));
+				}
+				printf("\n");
+			}
+			printf("\n");
+		}
 
 	} else {
 		fprintf (stderr,"No graph given, you need to use the -g option.\n");
@@ -186,8 +257,8 @@ int main( int argc, char* argv[] )
 	const char * entity="broadcast";
 	const char * entityfile="broadcast.cl";
 
-	// Node numbers
-	unsigned int nodes = 5;
+//	// Node numbers
+//	unsigned int nodes = 5;
 
 	// Message types
 	unsigned int messtypes = 1;
@@ -195,17 +266,6 @@ int main( int argc, char* argv[] )
 	// Node registers
 	unsigned int registers = 1;
  
- 	// The links matrix is a nodes x nodes which stores 0 if there in not a link x->y, !=0 otherwise (in the future it may contain same sort of link weight)
-//	int * links;
-
-	int links[]={
-			0, 1, 0, 0, 0,
-			1, 0, 1, 0, 0,
-			0, 1, 0, 1, 1,
-			0, 0, 1, 0, 0,
-			0, 0, 1, 0, 0
-	};
-
  	// The states matrix is a nodes x registers which stores for each entity the value of the given register. Two things:
  	// 1 - The register value if up to the opencl kernel, it is relevant to the host only for analysis purposes.
  	// 2 - The first register is the state register and has to exist.
@@ -217,20 +277,15 @@ int main( int argc, char* argv[] )
 
 	///////// The configuration part stops here
 
-	int i,j,k,l;
 	int doneck;
 
 	long int messcompl=0;
 
-	// Size, in bytes, of each vector
-	size_t bytes = sizeof(int);
  
 	// Allocate memory for each vector on host
-//	links = (int*)malloc(nodes*nodes*bytes);
 	states = (int*)malloc(nodes*registers*bytes);
 	messages = (int*)malloc(nodes*nodes*messtypes*bytes);
 
-//	for (i=0;i<nodes*nodes;i++) *(links+i)=0;
 	for (i=0;i<nodes*registers;i++) *(states+i)=0;
 	for (i=0;i<nodes*nodes*messtypes;i++) *(messages+i)=0;
 
@@ -334,21 +389,21 @@ int main( int argc, char* argv[] )
 	// Write our data set into the input array in device memory
 	err = clEnqueueWriteBuffer(queue, d_links, CL_TRUE, 0, nodes*nodes*bytes, links, 0, NULL, NULL);
 
-//*(states)=1;
+*(states)=1;
 //*(states+4)=1;
-	for (i=0;i<nodes;i++) {
-		*(states+i)=1;
-	}
+//	for (i=0;i<nodes;i++) {
+//		*(states+i)=1;
+//	}
 
 	err |= clEnqueueWriteBuffer(queue, d_states, CL_TRUE, 0, nodes*registers*bytes, states, 0, NULL, NULL);
-//*(messages)=1;
+*(messages)=1;
 //*(messages+24)=1;
 
-	for (i=0;i<nodes;i++) {
-		for (j=0;j<nodes;j++) {
-			if (i==j) *(messages+i*nodes+j)=1;
-		}
-	}
+//	for (i=0;i<nodes;i++) {
+//		for (j=0;j<nodes;j++) {
+//			if (i==j) *(messages+i*nodes+j)=1;
+//		}
+//	}
 
 	err |= clEnqueueWriteBuffer(queue, d_messages_in, CL_TRUE, 0,  nodes*nodes*messtypes*bytes, messages, 0, NULL, NULL);
 	for (i=0;i<nodes*nodes;i++) {
@@ -362,73 +417,78 @@ int main( int argc, char* argv[] )
 		return EXIT_FAILURE;
 	}
 
-for (l=0;l<6;l++) {
- 
-	// Set the arguments to our compute kernel
-	err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_links);
-	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_states);
-	err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_messages_in);
-	err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &d_messages_out);
-	err |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &messtypes);
-	err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &registers);
-	err |= clSetKernelArg(kernel, 6, sizeof(unsigned int), &nodes);
-	if (err != CL_SUCCESS)
-	{
-		fprintf(stderr, "Failed to set kernel arguments! %d\n", err);
-		return EXIT_FAILURE;
-	}
- 
-	// Execute the kernel over the entire range of the data set 
-	err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
-	if (err != CL_SUCCESS)
-	{
-		fprintf(stderr, "Failed to execute kernel!\n");
-		return EXIT_FAILURE;
-	}
- 
-	// Wait for the command queue to get serviced before reading back results
-	clFinish(queue);
+	if (verbose) printf("Staring simulation:\n",l);
 
-	// Read the results from the device
-	err=clEnqueueReadBuffer(queue, d_states, CL_TRUE, 0, nodes*registers*bytes, states, 0, NULL, NULL );
-	err|=clEnqueueReadBuffer(queue, d_messages_out, CL_TRUE, 0, nodes*nodes*messtypes*bytes, messages, 0, NULL, NULL );
+	// Main sumoulation cycle
+	for (l=0;l<1000;l++) {
 
-	if (err != CL_SUCCESS)
-	{
-		fprintf(stderr, "Failed to read output array! %d\n", err);
-		return EXIT_FAILURE;
-	}
-
-doneck=1;
-	for (i=0 ; i < nodes ; i++) {
-		printf("Node %d registers: ",i);
-		for (j=0 ; j < registers ; j++) {
-			printf("%d ",*(states+i*registers+j));
+		if (verbose) printf(" - Time %d:\n",l);
+ 
+		// Set the arguments to our compute kernel
+		err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_links);
+		err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_states);
+		err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_messages_in);
+		err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &d_messages_out);
+		err |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &messtypes);
+		err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &registers);
+		err |= clSetKernelArg(kernel, 6, sizeof(unsigned int), &nodes);
+		if (err != CL_SUCCESS)
+		{
+			fprintf(stderr, "Failed to set kernel arguments! %d\n", err);
+			return EXIT_FAILURE;
 		}
-		printf("\n");
-
-		printf("Node %d messages: ",i);
-		for (j=0 ; j < nodes ; j++) {
-			for (k=0 ; k < messtypes ; k++) {
-				printf("( %d -> %d = %d ) ",i,j,*(messages+(i*nodes+j)*messtypes+k));
-				if ( *(messages+(i*nodes+j)*messtypes+k)==1) messcompl++;
+	 
+		// Execute the kernel over the entire range of the data set 
+		err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+		if (err != CL_SUCCESS)
+		{
+			fprintf(stderr, "Failed to execute kernel!\n");
+			return EXIT_FAILURE;
+		}
+	 
+		// Wait for the command queue to get serviced before reading back results
+		clFinish(queue);
+	
+		// Read the results from the device
+		err=clEnqueueReadBuffer(queue, d_states, CL_TRUE, 0, nodes*registers*bytes, states, 0, NULL, NULL );
+		err|=clEnqueueReadBuffer(queue, d_messages_out, CL_TRUE, 0, nodes*nodes*messtypes*bytes, messages, 0, NULL, NULL );
+	
+		if (err != CL_SUCCESS)
+		{
+			fprintf(stderr, "Failed to read output array! %d\n", err);
+			return EXIT_FAILURE;
+		}
+	
+		doneck=1;
+		for (i=0 ; i < nodes ; i++) {
+			if (verbose) {
+				printf("   Node %s registers: ",(*(ithnode+i))->name);
+				for (j=0 ; j < registers ; j++) {
+					printf("%d ",*(states+i*registers+j));
+				}
+				printf("\n");
+	
+				printf("   Node %s messages: ",(*(ithnode+i))->name);
+				for (j=0 ; j < nodes ; j++) {
+					for (k=0 ; k < messtypes ; k++) {
+						printf("( %d -> %d = %d ) ",i,j,*(messages+(i*nodes+j)*messtypes+k));
+						if ( *(messages+(i*nodes+j)*messtypes+k)==1) messcompl++;
+					}
+				}
+				printf("\n");
+			}
+			if (*(states+i*registers+0)!=-1) {
+				doneck=0;
 			}
 		}
 		printf("\n");
-
-		if (*(states+i*registers+0)!=-1) {
-			doneck=0;
-		}
+	
+		if (doneck==1) break;
+	
+		tempm=d_messages_in;
+		d_messages_in=d_messages_out;
+		d_messages_out=tempm;
 	}
-	printf("\n");
-
-	if (doneck==1) break;
-
-	tempm=d_messages_in;
-	d_messages_in=d_messages_out;
-	d_messages_out=tempm;
-
-}
 
 	printf("Time Complexity: %d\n",l+1);
 	printf("Message Complexity: %ld\n",messcompl);
